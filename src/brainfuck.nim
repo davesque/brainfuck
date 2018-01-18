9afcfcf4113bc1da5b60593e27e3c2fcd1af8e0b
David Sanders <davesque@gmail.com>
2018-01-18 14:29:36 -0700

    Update brainfuck interpreter

--- src/brainfuck.nim
+++ src/brainfuck.nim
@@ -1,2 +1,51 @@
-# Hello Nim!
-echo "Hello, World!"
+{.push overflowchecks: off.}
+proc xinc(c: var char) = inc c
+proc xdec(c: var char) = dec c
+{.pop.}
+
+
+proc interpret*(code: string) =
+  var
+    tape = newSeq[char]()
+    codePos = 0
+    tapePos = 0
+
+  proc run(skip = false): bool =
+    while tapePos >= 0 and codePos < code.len:
+      if tapePos >= tape.len:
+        tape.add '\0'
+
+      if code[codePos] == '[':
+        inc codePos
+        let oldPos = codePos
+        while run(tape[tapePos] == '\0'):
+          codePos = oldPos
+
+      elif code[codePos] == ']':
+        return tape[tapePos] != '\0'
+
+      elif not skip:
+        case code[codePos]
+        of '+': xinc tape[tapePos]
+        of '-': xdec tape[tapePos]
+        of '>': inc tapePos
+        of '<': dec tapePos
+        of '.': stdout.write tape[tapePos]
+        of ',': tape[tapePos] = stdin.readChar
+        else: discard
+
+      inc codePos
+
+  discard run()
+
+
+when isMainModule:
+  import os
+
+  let code =
+    if paramCount() > 0:
+      readFile paramStr(1)
+    else:
+      readAll stdin
+
+  interpret code
